c2120c7634fc6d417c46b560566e0a7193ca6c9e
kingsleychuuu <kingsleychu0730@hotmail.com>
2020-05-10 15:27:30 -0400

    layout ui and added settings

--- SwiftAdBlocker/ViewController.swift
+++ SwiftAdBlocker/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let cellID = "cellID"
+    
+    lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.backgroundColor = .green
+        cv.dataSource = self
+        cv.delegate = self
+        return cv
+    }()
+    
+    let buttonStackView = UIStackView()
+    lazy var settingsButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.setTitle("Settings", for: .normal)
+        button.addTarget(self, action: #selector(settingsButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    lazy var nextButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.setTitle("Next", for: .normal)
+        button.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+        
+        view.addSubview(collectionView)
+        view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(settingsButton)
+        buttonStackView.addArrangedSubview(nextButton)
+        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            settingsButton.widthAnchor.constraint(equalTo: nextButton.widthAnchor),
+            
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: buttonStackView.topAnchor),
+            
+            buttonStackView.heightAnchor.constraint(equalToConstant: 50),
+            buttonStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            buttonStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            buttonStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    @objc func settingsButtonTapped() {
+        guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+
+        if UIApplication.shared.canOpenURL(settingsUrl) {
+            UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                print("Settings opened: \(success)") // Prints true
+            })
+        }
+    }
+    
+    @objc func nextButtonTapped() {
+        
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        return cell
+    }
+    
+    
 }
 
